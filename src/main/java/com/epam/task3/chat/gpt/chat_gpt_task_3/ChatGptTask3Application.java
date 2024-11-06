d634a527845e0b174ebe5a860bdd34455a13d7f1
Stanislav Donetc <stanislavdonetc@gmail.com>
2024-11-06 14:56:03 +0200

    initial commit

---
+++ src/main/java/com/epam/task3/chat/gpt/chat_gpt_task_3/ChatGptTask3Application.java
@@ -0,0 +1,13 @@
+package com.epam.task3.chat.gpt.chat_gpt_task_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGptTask3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatGptTask3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/task3/chat/gpt/chat_gpt_task_3/ChatGptTask3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.task3.chat.gpt.chat_gpt_task_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGptTask3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
